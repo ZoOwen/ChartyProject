3c10ce91a60289cc00923f5c4a0c691d8bfef4b2
zoOwen <raiffahmi2001@gmail.com>
2020-05-08 20:41:20 +0700

    mindahin file

---
+++ boilerplate/src/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import Admin from "./Admin";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Login from "./Login";
+var isLogin = "admins";
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Login />
+        </Route>
+        <Route path="/home">{isLogin == "admin" ? <Admin /> : <App />}</Route>
+      </Switch>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
