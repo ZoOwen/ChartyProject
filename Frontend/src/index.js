3a895f299257fd143b3a90544bc0b59c4089e786
zoOwen <raiffahmi2001@gmail.com>
2020-05-09 14:25:55 +0700

    push footer

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -6,15 +6,16 @@ import * as serviceWorker from "./serviceWorker";
 import Admin from "./Admin";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import SignIn from "./pages/SignIn";
+import "bootstrap/dist/css/bootstrap.min.css";
 var isLogin = "admins";
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Switch>
-        <Route exact path="/">
+        <Route exact path="/login">
           <SignIn />
         </Route>
-        <Route path="/home">{isLogin == "admin" ? <Admin /> : <App />}</Route>
+        <Route path="/">{isLogin == "admin" ? <Admin /> : <App />}</Route>
       </Switch>
     </Router>
   </React.StrictMode>,
