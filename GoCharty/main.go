97d8b91547486d7eee88eb004ee60d3617468c10
jumadimuhammad <getjumadi@gmail.com>
2020-05-09 20:49:07 +0700

    create backend user save

--- GoCharty/main.go
+++ GoCharty/main.go
@@ -5,24 +5,46 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+  "github.com/zoowen/model"
+  "github.com/go-sql-driver/mysql"
+	"github.com/jumadimuhammad/http-request/model"
+	"github.com/labstack/echo"
 )
 
-func main() {
-  // Echo instance
-  e := echo.New()
+func app(e *echo.Echo, store model.UserStore) {
+  e.POST("/user", func(c echo.Context) error {
+		// Given
+		title := c.FormValue("title")
+		body := c.FormValue("body")
 
-  // Middleware
-  e.Use(middleware.Logger())
-  e.Use(middleware.Recover())
+		// Create instance
+		user, _ := model.CreateUser(title, body)
 
-  // Routes
-  e.GET("/", hello)
+		// Persist
+		store.Save(user)
 
-  // Start server
-  e.Logger.Fatal(e.Start(":1323"))
+		// Response
+		return c.JSON(http.StatusOK, user)
+	})
 }
 
-// Handler
-func hello(c echo.Context) error {
-  return c.String(http.StatusOK, "Hello, World!")
+func main() {
+	// Echo instance
+	e := echo.New()
+
+	var store model.UserStore
+	store = model.NewUserMySQL()
+
+	// // Middleware
+	// e.Use(middleware.Logger())
+	// e.Use(middleware.Recover())
+
+	// // Routes
+	// e.GET("/", hello)
+  // e.POST("/user", store)
+  
+  app(e, store)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":8080"))
 }
