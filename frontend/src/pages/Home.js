3b3a1eb25ab88f26a596a0338406c774455bfc6c
rubianggoro <rubi.anggoro199@gmail.com>
2020-05-11 21:48:35 +0700

    tambah testimonials

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -5,6 +5,7 @@ import Carousel from "../components/Home/CarouselHome";
 import Section1 from "../components/Home/Section1Home";
 import Recent from "../components/Home/RecentHome";
 import Section2 from "../components/Home/Section2Home";
+import Testimonials from "../components/Home/Testimonials";
 import Footer from "../components/Footer";
 
 function Home() {
@@ -14,6 +15,7 @@ function Home() {
       <Section1 />
       <Recent />
       <Section2 />
+      <Testimonials />
       <Footer />
     </div>
   );
