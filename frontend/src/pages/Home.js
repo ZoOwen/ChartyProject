2490c60c6c7ac9d1bce7b46528e8780c98ff26aa
rubianggoro <rubi.anggoro199@gmail.com>
2020-05-11 12:02:30 +0700

    tambhan section

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 
 // Components
 import Carousel from "../components/Home/CarouselHome";
+import Section1 from "../components/Home/Section1Home";
 import Footer from "../components/Footer";
 
 function Home() {
   return (
     <div>
       <Carousel />
+      <Section1 />
       <Footer />
     </div>
   );
