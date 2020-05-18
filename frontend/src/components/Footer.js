10e368190fdd62e1b414ff82ccc744e90f6e2778
jumadimuhammad <getjumadi@gmail.com>
2020-05-18 16:49:11 +0700

    perbaikan footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,70 +1,77 @@
+/** @format */
+
 import React from "react";
 import "../css/Footer.css";
+import fb from "../assets/sosmed/facebook.svg";
+import tw from "../assets/sosmed/twitter.svg";
+import ig from "../assets/sosmed/instagram.svg";
+import yt from "../assets/sosmed/youtube.svg";
+import pt from "../assets/sosmed/pinterest.svg";
+import lk from "../assets/sosmed/linkedin.svg";
+
 function Footer() {
-  return (
-    <div>
-      <div
-        className=" container-fluid  wrapper hidden"
-        style={{ background: "#363636" }}
-      >
-        <br />
-        <div className="row" style={{ marginLeft: "50px" }}>
-          <div className="col-md-3">
-            <img
-              src="https://charity.w3itexperts.com/images/logo.png"
-              alt="img"
-            />
+    return (
+        <>
+            <div
+                className=" container-fluid  wrapper hidden"
+                style={{ background: "#363636" }}
+            >
+                <div className=" container mt-3">
+                    <div className="row">
+                        <div className="col-md-3">
+                            <img
+                                src="https://charity.w3itexperts.com/images/logo.png"
+                                alt="img"
+                            />
 
-            <p className="fcontent">
-              Charity Lorem Ipsum is dummy text ever since the been when an
-              unknown printer. simply dummy text of the printing and typesetting
-              industry has been the industry Lorem Ipsum is dummy text ever
-              since the been when an unknown printer. simply dummy text of the
-              printing and typesetting industry has been the industry
-            </p>
-          </div>
-          <div className="col-md-3">
-            <br />
-            <br />
-            <h2>Our Services</h2>
-            <hr />
-            <p>Food Services</p>
-            <p>Clear Water</p>
-            <p>Education</p>
-            <p>Donation</p>
-          </div>
-          <div className="col-md-3">
-            <br />
-            <br />
-            <h2>Contact Info</h2>
-            <hr />
-            <p>Address: - 786 Dummy Road, City Name, Country</p>
+                            <p className="mt-2">
+                              Charity adalah sebuah situs website 
+                            </p>
+                        </div>
+                        <div className="col-md-3">
+                            <h4>Charity</h4>
+                            <hr />
+                            <p>Tentang charity</p>
+                            <p>Seminar dan pelatihan</p>
+                            <p>Pendidikan</p>
+                            <p>Donasi</p>
+                        </div>
+                        <div className="col-md-3">
+                            <h4>Info kontak</h4>
+                            <hr />
+                            <p>Phone : 021-123-7890</p>
+                            <p>E-mail : info@charity.com</p>
+                            <p>Address : Jalan Raya Kemang No.10, Jakarta Selatan, Jakarta-Indonesia</p>
+                        </div>
+                        <div className="col-md-3">
+                            <h4>Sosial media</h4>
+                            <hr />
+                            <div className="sosmed">
+                                <img src={fb} alt="Facebook" />
+                                <img src={tw} alt="Twitter" />
+                                <img src={ig} alt="Instagram" />
+                                <img src={pt} alt="Pinterest" />
+                                <img src={lk} alt="Linkedin" />
+                                <img src={yt} alt="Youtube" />
+                            </div>
+                        </div>
+                    </div>
 
-            <p>Phone: - +1 123-456-7890 or +1 123-456-7890</p>
-
-            <p>E-mail: - example@gmail.com example@gmail.com</p>
-          </div>
-          <div className="col-md-3">
-            <br />
-            <br />
-            <br />
-            <br />
-            <h2>Social Link</h2>
-          </div>
-        </div>
-
-        <br />
-      </div>
-      <div
-        className=" wrapper"
-        style={{ background: "#F75D08", marginTop: "-5px" }}
-      >
-        &nbsp; &nbsp; &nbsp;
-        <p className="copyright">Reserved CopyRight 2020 Charity</p>
-        &nbsp;
-      </div>
-    </div>
-  );
+                    <br />
+                </div>
+            </div>
+            <div className="container-fluid" style={{ background: "#F75D08" }}>
+                <div
+                    className=" wrapper"
+                    // style={{ background: "#F75D08", marginTop: "-5px" }}
+                >
+                    &nbsp; &nbsp; &nbsp;
+                    <p className="copyright">Reserved CopyRight 2020 Charity</p>
+                    &nbsp;
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default Footer;
